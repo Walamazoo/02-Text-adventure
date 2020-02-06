c9a8e5da60dc5b79f1b19575d971806674f1e377
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2020-02-05 20:56:55 -0600

    Fullfills the requiremnets of the assignment.

--- text-adventure/main.py
+++ text-adventure/main.py
@@ -4,30 +4,74 @@ import sys, os, json
 assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 # The game and item description files (in the same folder as this script)
-game_file = 'zork.json'
-item_file = 'items.json'
+game_file = "haunted.json"
 
 
-# Load the contents of the files into the game and items dictionaries. You can largely ignore this
-# Sorry it's messy, I'm trying to account for any potential craziness with the file location
+# Load the contents of the files into the game and items dictionaries
 def load_files():
     try:
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         with open(os.path.join(__location__, game_file)) as json_file: game = json.load(json_file)
-        with open(os.path.join(__location__, item_file)) as json_file: items = json.load(json_file)
-        return (game,items)
+        return game
     except:
         print("There was a problem reading either the game or item file.")
         os._exit(1)
 
+
+def render(game,current):
+    c = game[current]
+    print("\nThis is the " + c["name"])
+    print(c["desc"])
+
+    if current != "END" and current != "DEAD":
+        print("\nAvaliable exits: ")
+        for e in c["exits"]:
+            print(e["exit"].lower())
+    
+
+def get_input():
+    response = input("Where will you go? ")
+    response = response.upper().strip()
+    return response
+
+def update(game,current,response):
+    c = game[current]
+    for e in c["exits"]:
+        if response == e["exit"]:
+            return e["target"]
+
+    else:
+        print("That won't work. Try something else.")
+
+    return current
+
 # The main function for the game
 def main():
-    current = 'WHOUS'  # The starting location
+    current = 'FOYER'  # The starting location
     end_game = ['END']  # Any of the end-game locations
+    dead_end = ['DEAD']
 
-    (game,items) = load_files()
+    game = load_files()
 
-    # Add your code here
+    while True:
+        render(game,current)
+
+        if current in end_game:
+            print("You win!")
+            break
+
+        if current in dead_end:
+            print("You died!")
+            break
+
+        response = get_input()
+
+        if response == "QUIT" or response == "Q":
+            break 
+
+        current = update(game,current,response)
+
+    print("Thanks for playing!")
 
 # run the main function
 if __name__ == '__main__':
